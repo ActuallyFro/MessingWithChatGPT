6648d24cca31636175d865f3f2a0208ff1fbc90a
actuallyfro <actuallyfro@github.com>
2023-03-11 20:23:39 -0600

    {14_RCV-tailored} Got single winner implemented

--- 14_RCV-tailored/src/main.rs
+++ 14_RCV-tailored/src/main.rs
@@ -4,6 +4,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+#[derive(Clone)]
 struct Ballot {
     uuid: String,
     choices: Vec<String>,
@@ -95,23 +96,31 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("[DEBUG] Total Choices: {}", discovered_choices.len());
     println!("[DEBUG] Discovered Choices: {:?}", discovered_choices);
 
-    let debug_breakout = true;
+    let debug_breakout = false;
+    // clone the ballots vector for the RCV algorithm
+    let mut ballots_rcv_analysis = ballots.clone();
+
+    // // List Top-to-Bottom of Winners -- as choices are removed they are pushed onto this vector of strings
+    let mut TopToBottomList = Vec::new();
+
+    let mut RCV_Downselect_Loop_Counter = 0;
     loop {
+        println!("[DEBUG] RCV Loop Iteration: {}", RCV_Downselect_Loop_Counter);
 
         // // Count the first choice votes
         let mut counter = 0;
 
         let mut vote_counts = HashMap::new();
-        for ballot in &ballots {
-            println!("[DEBUG] Ballot #{}", counter);
-            println!("[DEBUG] UUID: {}", ballot.uuid);
-            println!("[DEBUG] Choice: {}", ballot.choices[0]);
+        for ballot in &ballots_rcv_analysis {
+            // println!("[DEBUG] Ballot #{}", counter);
+            // println!("[DEBUG] UUID: {}", ballot.uuid);
+            // println!("[DEBUG] Choice: {}", ballot.choices[0]);
 
             let count = vote_counts.entry(ballot.choices[0].clone()).or_insert(0);
             *count += 1;
 
             counter += 1;
-            println!("");
+            // println!("");
         }
 
         println!("[DEBUG] Vote Counts: {:?}", vote_counts);
@@ -123,6 +132,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         //Calculate the highest vote count in vote_counts
         let mut largest_ballot_sum = 0;
         let mut largest_ballot_choice = String::new();
+
         for (key, value) in &vote_counts {
             if value > &largest_ballot_sum {
                 largest_ballot_sum = *value;
@@ -136,78 +146,60 @@ fn main() -> Result<(), Box<dyn Error>> {
         // evlauate if largest_ballot_sum > 50% of total_ballot_votes
         if largest_ballot_sum > total_ballot_votes / 2 {
             println!("[DEBUG] Winner: {}", largest_ballot_choice);
+            println!("[DEBUG] % of vote: {}", largest_ballot_sum as f32 / total_ballot_votes as f32 * 100.0);
+            //push the winner onto the TopToBottomList
+            TopToBottomList.push(largest_ballot_choice.clone());
+            break;
+
         } else {
             println!("[DEBUG] No winner yet");
             println!("[DEBUG] % of vote: {}", largest_ballot_sum as f32 / total_ballot_votes as f32 * 100.0);
         }
 
-        // breakout for now...
-        if debug_breakout {
-            break;
+        let mut smallest_ballot_sum = 0;
+        let mut smallest_ballot_choice = String::new();
+        //Determine smallest ballot sum
+        for (key, value) in &vote_counts {
+            println!("[DEBUG] Key: {}", key);
+
+            if value < &smallest_ballot_sum || smallest_ballot_sum == 0{
+                println!("[DEBUG] Value: {}", value);
+                smallest_ballot_sum = *value;
+                smallest_ballot_choice = key.clone();
+            }
         }
+        //debug
+        println!("[DEBUG] Smallest Ballot Sum: {}", smallest_ballot_sum);
+        println!("[DEBUG] Smallest Ballot Choice: {}", smallest_ballot_choice);
+
+        // loop over vote_counts, if the value is equal to smallest_ballot_sum, remove ballot.choices[0]
+        for (key, value) in &vote_counts {
+            // new String variable called eval_choice
+            let eval_choice = String::from(key);
+            if value == &smallest_ballot_sum {
+                for ballot in &mut ballots_rcv_analysis {
+                    if ballot.choices[0] == eval_choice {
+                        println!("[DEBUG] Removing: {} from {}", key, ballot.uuid);
+                        ballot.choices.remove(0);
+                    }
+                }
+            }
+        } 
+
+        // // breakout for now...
+        // if debug_breakout || RCV_Downselect_Loop_Counter > 15{
+        //     break;
+        // }
+        RCV_Downselect_Loop_Counter += 1;
+        println!("[DEBUG] ");
+
     }
 
 
+    println!("[DEBUG] TopToBottomList: {:?}", TopToBottomList);
 
 
 
-    // for ballot in &choices {
-    //     if let Some(choice) = ballot.first() {
-    //         let count = vote_counts.entry(choice.clone()).or_insert(0);
-    //         *count += 1;
-    //     }
-    // }
-
-    // let mut winners = vec![String::new(); choices[0].len()];
-    // let mut losers = HashSet::new();
-    // //Not used: let num_ballots = choices.len();
-
-    // loop {
-    //     let max_votes = vote_counts.values().max().ok_or("No max vote count found")?;
-    //     let mut top_choices: Vec<_> = vote_counts
-    //         .iter()
-    //         .filter(|(_, count)| **count == *max_votes)
-    //         .map(|(choice, _)| choice.clone())
-    //         .collect();
-    //     top_choices.sort();
-    //     let winner = top_choices.first().ok_or("No winner found")?.clone();
-    //     let winner_index = choices[0].iter().position(|c| *c == winner).ok_or("Winner not found in ballot choices")?;
-    //     winners[winner_index] = winner.clone();
-
-    //     if winners.iter().filter(|w| w.is_empty()).count() == 1 {
-    //         let loser_index = winners.iter().position(|w| w.is_empty()).unwrap();
-    //         let loser = choices[0][loser_index].clone();
-    //         losers.insert(loser.clone());
-
-    //         for ballot in &choices {
-    //             let mut new_choice = None;
-    //             for choice in ballot.iter().skip(1) {
-    //                 if new_choice.is_none() && !losers.contains(choice) {
-    //                     new_choice = Some(choice.clone());
-    //                 }
-    //                 if winners.iter().any(|w| *w == *choice) {
-    //                     let count = vote_counts.entry(choice.clone()).or_insert(0);
-    //                     *count += 1;
-    //                     break;
-    //                 }
-    //             }
-    //             if let Some(choice) = new_choice {
-    //                 let count = vote_counts.entry(choice.clone()).or_insert(0);
-    //                 *count += 1;
-    //             }
-    //         }
-
-    //         if vote_counts.values().all(|count| *count == 0) {
-    //             break;
-    //         }
-    //     } else {
-    //         break;
-    //     }
-    // }
-
-    // for (rank, winner) in winners.iter().enumerate() {
-    //     println!("Rank {}: {}", rank + 1, winner);
-    // }
 
     Ok(())
 }
