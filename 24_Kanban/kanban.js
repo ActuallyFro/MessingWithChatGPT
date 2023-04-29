19e737cecb5af6ed8e7050e3819e875a6bf16d1b
actuallyfro <actuallyfro@github.com>
2023-04-29 08:52:15 -0500

    {24_Kanban} HACKS!

--- 24_Kanban/kanban.js
+++ 24_Kanban/kanban.js
@@ -1,136 +1,312 @@
-const board = document.getElementById('board');
-const addBucketBtn = document.getElementById('add-bucket-btn');
-const exportJsonBtn = document.getElementById('export-json-btn');
-const importJsonBtn = document.getElementById('import-json-btn');
-const resetLocalStorageBtn = document.getElementById('reset-localstorage-btn');
-
-let bucketCount = 1;
-
-function saveBoardState() {
-    localStorage.setItem('kanbanBoard', board.innerHTML);
+// --------------------
+// localStorage Functions
+function saveToLocalStorage() {
+  // console.log("[DEBUG] LocalStorage() SAVING!")
+  const bucketsData = [];
+  document.querySelectorAll(".bucket").forEach((bucket, index) => {
+      const cards = Array.from(bucket.querySelectorAll(".card"));
+      const cardsData = cards.map(card => {
+          return {
+              label: card.querySelector("input[type='text']").value,
+              body: card.querySelector(".card-body").outerHTML
+          };
+      });
+      bucketsData[index] = cardsData;
+  });
+  localStorage.setItem("eisenhowerMatrix", JSON.stringify(bucketsData));
 }
 
-function loadBoardState() {
-    const savedBoard = localStorage.getItem('kanbanBoard');
-    if (savedBoard) {
-        board.innerHTML = savedBoard;
-    }
-}
+function loadFromLocalStorage() {
+  // console.log("[DEBUG] LocalStorage() LOADING!")
 
-function createTask(bucket) {
-    const task = document.createElement('div');
-    task.className = 'task';
-    task.textContent = 'Task ' + (bucket.getElementsByClassName('task').length + 1);
-    task.draggable = true;
-    task.addEventListener('dblclick', editTask);
-    task.addEventListener('dragstart', onDragStart);
-    return task;
-}
-
-function editTask(event) {
-    const task = event.target;
-    const taskText = prompt('Edit task:', task.textContent);
-    if (taskText) {
-        task.textContent = taskText;
-    }
-}
-
-function addTask(event) {
-  const bucket = event.target.parentNode;
-  const taskList = bucket.getElementsByClassName('task-list')[0];
-  const task = createTask(bucket);
-  taskList.appendChild(task);
-  saveBoardState();
-}
-
-function createBucket(bucketId, title) {
-  const bucket = document.createElement('div');
-  bucket.className = 'bucket';
-  bucket.id = bucketId;
-
-  const bucketTitle = document.createElement('h2');
-  bucketTitle.className = 'bucket-title';
-  bucketTitle.textContent = title;
-  bucketTitle.addEventListener('dblclick', editBucketTitle);
-  bucket.appendChild(bucketTitle);
-
-  const addTaskBtn = document.createElement('button');
-  addTaskBtn.className = 'add-task-btn';
-  addTaskBtn.textContent = 'Add Task';
-  addTaskBtn.addEventListener('click', addTask);
-  bucket.appendChild(addTaskBtn);
-
-  const taskList = document.createElement('div');
-  taskList.className = 'task-list';
-  taskList.addEventListener('dragover', onDragOver);
-  taskList.addEventListener('drop', onDrop);
-  bucket.appendChild(taskList);
-
-  return bucket;
-}
-
-function editBucketTitle(event) {
-  const bucketTitle = event.target;
-  const titleText = prompt('Edit bucket title:', bucketTitle.textContent);
-  if (titleText) {
-      bucketTitle.textContent = titleText;
-      saveBoardState();
+  const bucketsData = JSON.parse(localStorage.getItem("eisenhowerMatrix"));
+  if (bucketsData) {
+      bucketsData.forEach((bucketData, index) => {
+          const bucket = document.querySelectorAll(".bucket")[index];
+          const container = bucket.querySelector(".container");
+          bucketData.forEach(cardData => {
+              const card = createCard();
+              card.querySelector("input[type='text']").value = cardData.label;
+              card.querySelector(".card-body").outerHTML = cardData.body;
+              container.appendChild(card);
+              updateCardCount(bucket, 1);
+          });
+      });
   }
 }
-
-addBucketBtn.addEventListener('click', () => {
-  bucketCount++;
-  const bucketId = 'bucket-' + bucketCount;
-  const title = 'Bucket ' + bucketCount;
-  const bucket = createBucket(bucketId, title);
-  board.appendChild(bucket);
-  saveBoardState();
-});
-
-function onDragStart(event) {
-  event.dataTransfer.setData('text/plain', event.target.id);
-}
-
-function onDragOver(event) {
-  event.preventDefault();
-}
-
-function onDrop(event) {
-  const taskId = event.dataTransfer.getData('text');
-  const task = document.getElementById(taskId);
-  if (task && event.target.classList.contains('task-list')) {
-      event.target.appendChild(task);
-      saveBoardState();
-  }
-}
-
-exportJsonBtn.addEventListener('click', () => {
-  const json = JSON.stringify(board.innerHTML);
-  const a = document.createElement('a');
-  const file = new Blob([json], {type: 'application/json'});
-  a.href = URL.createObjectURL(file);
-  a.download = 'kanban-board.json';
-  a.click();
-});
-
-importJsonBtn.addEventListener('change', (event) => {
-  const file = event.target.files[0];
-  const reader = new FileReader();
-  reader.onload = (e) => {
-      const json = e.target.result;
-      board.innerHTML = JSON.parse(json);
-      saveBoardState();
-  };
-  reader.readAsText(file);
-});
-
 
 function resetLocalStorage() {
-  localStorage.removeItem('kanbanBoard');
+  // console.log("[DEBUG] LocalStorage() RESETTING!")
+
+  localStorage.removeItem("eisenhowerMatrix");
   location.reload();
 }
 
-resetLocalStorageBtn.addEventListener('click', resetLocalStorage);
+//==============================================================================================================
+// --------------------
+// Card and Bucket Functions
+document.querySelectorAll(".plus").forEach(plus => {
+  plus.addEventListener("click", (e) => {
+      const bucket = e.target.parentElement;
+      const container = bucket.querySelector(".container");
+      const card = createCard();
+      container.appendChild(card);
+      updateCardCount(bucket, 1);
+      saveToLocalStorage();
+  });
+});
+
+function createCard() {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.setAttribute("draggable", "true");
+
+  const cardLabelInput = document.createElement("input");
+  cardLabelInput.addEventListener("input", saveToLocalStorage);
+  cardLabelInput.type = "text";
+  cardLabelInput.placeholder = "Label...";
+  card.appendChild(cardLabelInput);
+
+  const minus = document.createElement("span");
+  minus.textContent = "-";
+  minus.className = "minus";
+  minus.addEventListener("click", () => {
+      const bucket = card.parentElement.parentElement;
+      card.remove();
+      updateCardCount(bucket, -1);
+  });
+
+  card.appendChild(minus);
+
+  const viewAsHtmlIcon = document.createElement("i");
+  viewAsHtmlIcon.className = "fas fa-eye view-as-html";
+  viewAsHtmlIcon.style.marginLeft = "0.5rem";
+  card.appendChild(viewAsHtmlIcon);
+
+  const editIcon = document.createElement("i");
+  editIcon.className = "fas fa-edit edit";
+  editIcon.style.display = "none";
+  editIcon.style.marginLeft = "0.5rem";
+  card.appendChild(editIcon);
+
+  let cardBody = document.createElement("textarea");
+  cardBody.className = "card-body";
+  cardBody.placeholder = "Enter description...";
+  
+  card.appendChild(cardBody);
+
+  viewAsHtmlIcon.addEventListener("click", () => {
+      if (cardBody.tagName === "TEXTAREA") {
+          const sanitizedHtml = sanitizeHTML(cardBody.value);
+          const renderedHtml = document.createElement("div");
+          renderedHtml.className = "card-body";
+          renderedHtml.innerHTML = sanitizedHtml;
+          card.replaceChild(renderedHtml, cardBody);
+          cardBody = renderedHtml;
+          viewAsHtmlIcon.style.display = "none";
+          editIcon.style.display = "inline";
+
+      } else {
+          const textarea = document.createElement("textarea");
+          textarea.className = "card-body";
+          textarea.placeholder = "Enter description...";
+          textarea.innerHTML = cardBody.innerHTML;
+          card.replaceChild(textarea, cardBody);
+          cardBody = textarea;
+
+          viewAsHtmlIcon.style.display = "inline";
+          editIcon.style.display = "none";
+      }
+  });
 
 
-loadBoardState();
+  editIcon.addEventListener("click", () => {
+      if (cardBody.tagName === "DIV") {
+          const textarea = document.createElement("textarea");
+          textarea.className = "card-body";
+          textarea.placeholder = "Enter description...";
+          textarea.value = cardBody.innerHTML; // Change this line
+          card.replaceChild(textarea, cardBody);
+          cardBody = textarea;
+
+          viewAsHtmlIcon.style.display = "inline";
+          editIcon.style.display = "none";
+
+      } else {
+          const newCardBody = document.createElement("div");
+          newCardBody.className = "card-body";
+          newCardBody.innerHTML = cardBody.value;
+          if (card.contains(cardBody)) {
+              card.replaceChild(newCardBody, cardBody);
+          } else {
+              card.appendChild(newCardBody);
+          }
+          cardBody = newCardBody;
+          viewAsHtmlIcon.style.display = "inline";
+          editIcon.style.display = "none";
+      }
+  });
+  
+  return card;
+}
+
+function updateCardCount(bucket, change) {
+  const counter = bucket.querySelector(".counter");
+  const newCount = parseInt(counter.textContent, 10) + change;
+  counter.textContent = newCount;
+}
+
+function sanitizeHTML(html) {
+  const temp = document.createElement("div");
+  temp.innerHTML = html;
+  const tagsToKeep = ['b', 'u', 'i', 'strike', 'ul', 'li'];
+  const tags = temp.getElementsByTagName("*");
+
+  for (let i = tags.length - 1; i >= 0; i--) {
+      const tag = tags[i];
+      if (tagsToKeep.indexOf(tag.tagName.toLowerCase()) === -1) {
+          tag.outerHTML = tag.innerHTML;
+      }
+  }
+  return temp.innerHTML;
+}
+
+document.querySelectorAll(".bucket").forEach(bucket => {
+  bucket.addEventListener("dragover", (e) => {
+      e.preventDefault();
+  });
+
+  // Update the 'drop' event listener to update the card counts
+  bucket.addEventListener("drop", (e) => {
+      e.preventDefault();
+      const cardId = e.dataTransfer.getData("text");
+      const card = document.getElementById(cardId);
+      const container = bucket.querySelector(".container");
+                  
+      if (card) {
+          const oldBucket = card.parentElement.parentElement;
+          container.appendChild(card);
+          updateCardCount(oldBucket, -1); // Decrease the card count in the old bucket by 1
+          updateCardCount(bucket, 1); // Increase the card count in the new bucket by 1
+      } else {
+          const draggedCard = document.querySelector('.dragging');
+          if (draggedCard) {
+              const oldBucket = draggedCard.parentElement.parentElement;
+              container.appendChild(draggedCard);
+              updateCardCount(oldBucket, -1); // Decrease the card count in the old bucket by 1
+              updateCardCount(bucket, 1); // Increase the card count in the new bucket by 1
+          }
+      }
+  });
+
+
+  bucket.addEventListener("dragenter", (e) => {
+      e.preventDefault();
+  });
+});
+//==============================================================================================================
+// Touch Events for Mouse-based events
+document.addEventListener("dragstart", (e) => {
+  if (e.target.classList.contains("card")) {
+      e.target.classList.add("dragging");
+      e.dataTransfer.setData("text", e.target.id);
+  }
+});
+
+document.addEventListener("dragend", (e) => {
+  if (e.target.classList.contains("card")) {
+      e.target.classList.remove("dragging");
+  }
+  saveToLocalStorage();
+});
+
+// --------------------
+// Touch Events for Mobile/Touchscreen Devices
+
+let draggedCard = null;
+let StartingBucket = null;
+
+
+document.addEventListener("touchstart", (e) => {
+if (e.target.classList.contains("card")) {
+  draggedCard = e.target;
+  StartingBucket = draggedCard.parentElement.parentElement;
+}
+});
+
+document.addEventListener("touchmove", (e) => {
+if (draggedCard) {
+  e.preventDefault();
+  draggedCard.style.position = "absolute";
+  draggedCard.style.left = e.touches[0].clientX + "px";
+  draggedCard.style.top = e.touches[0].clientY + "px";
+}
+});
+
+document.addEventListener("touchend", (e) => {
+  if (draggedCard) {
+    const touchX = e.changedTouches[0].clientX;
+    const touchY = e.changedTouches[0].clientY;
+    const bucket = document.elementFromPoint(touchX, touchY).closest(".bucket");
+    if (bucket) {
+      const container = bucket.querySelector(".container");
+      container.appendChild(draggedCard);
+      const newBucket = draggedCard.parentElement.parentElement;
+      updateCardCount(StartingBucket, -1);
+      updateCardCount(newBucket, 1);
+    }
+    draggedCard.style.position = "";
+    draggedCard.style.left = "";
+    draggedCard.style.top = "";
+    draggedCard = null;
+    saveToLocalStorage();
+  }
+});
+
+
+function findBucket(x, y) {
+const buckets = document.querySelectorAll(".bucket");
+for (let i = 0; i < buckets.length; i++) {
+  const rect = buckets[i].getBoundingClientRect();
+  if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+    return buckets[i];
+  }
+}
+return null;
+}
+
+
+
+loadFromLocalStorage();
+
+// Export function
+function exportMatrix() {
+  const bucketsData = JSON.parse(localStorage.getItem("eisenhowerMatrix")) || [];
+  const dataStr = JSON.stringify(bucketsData);
+  const dataBlob = new Blob([dataStr], { type: 'application/json;charset=utf-8' });
+  const dataUrl = URL.createObjectURL(dataBlob);
+
+  const link = document.createElement('a');
+  link.href = dataUrl;
+  link.download = 'eisenhower_matrix.json';
+  link.click();
+}
+
+// Import function
+function importMatrix(event) {
+  const file = event.target.files[0];
+  if (!file) return;
+
+  const reader = new FileReader();
+  reader.onload = function(e) {
+      const contents = e.target.result;
+      try {
+          const parsedData = JSON.parse(contents);
+          localStorage.setItem("eisenhowerMatrix", JSON.stringify(parsedData));
+          location.reload();
+      } catch (e) {
+          alert('Invalid JSON file!');
+      }
+  };
+  reader.readAsText(file);
+}
--- 24_Kanban/prompt.md
+++ 24_Kanban/prompt.md
@@ -20,7 +20,13 @@ It has 3 source files:
 Issue: LocalStorage needs to be clearable.
 
 Provide: edits to the code that adds "Reset LocalStorage" button.
+
+
+
 =================================
+
+
+
 The following SPA has 3 source files:
 ```html
 ``` 
@@ -34,3 +40,5 @@ The following SPA has 3 source files:
 Issue: a Kanban board is needed, but MOST of the features are already implemented in this Eisenhower Matrix.
 
 Provide modified code  to create a Kanban board. Replace the quadrants to have expandable and re-label-able buckets (or swim lanes). Also, the swim-lanes should have '<' and '>' symbols, left of the existing '+' symbol to allow reordering of the swim-lanes.
+
+=================================
